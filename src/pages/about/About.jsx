bb11119a33ea466a1b405f1c94e4a45750351fa9
Utsav <utsavjr228@gmail.com>
2025-01-20 14:19:38 +0545

    Changes in about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,41 +1,65 @@
+import { faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import NavFooter from "../../components/NavFooter/NavFooter";
-import "./about.css";
+import "./About.css";
 
 const About = () => {
   return (
     <>
+      <NavBar />
       <div>
-        <NavBar />
-        <div className="About-dive">
-          <div className="Mission">
+        <div className="AboutDiv">
+          <div className="About">
             <h2>ABOUT</h2>
-            <img src="finalLogo.png" className="logo3" alt="logo" />
-            <p>
-              " Movie Snacks is your go-to web app for
-              <br /> seamless cinema snacks ordering, ensuring
-              <br />
-              you never miss a moment of the movie !! "
-            </p>
-          </div>
-
-          <div className="border-center"></div>
-
-          <div className="arrow">
-            <h2>MISSION</h2>
-            <div className="About-mission-image ">
-              <img src="mission.png" className="logo4" alt="logo" />
+            <div className="AboutLogo">
+              <p>
+                <FontAwesomeIcon
+                  icon={faQuoteLeft}
+                  style={{ position: "relative", top: "-20px", left: "-20px" }}
+                />
+                Movie Snacks is your go-to web app for
+                <br /> seamless cinema snacks ordering, ensuring
+                <br />
+                you never miss a moment of the movie.
+                <FontAwesomeIcon
+                  icon={faQuoteRight}
+                  style={{
+                    position: "relative",
+                    bottom: "-80px",
+                    right: "-20px",
+                  }}
+                />
+              </p>
             </div>
-            <p>
-              " To enhance the movie experience by
-              <br />
-              simplifying snack ordering with technology !! "
-            </p>
+          </div>
+          <div className="BorderCenter"></div>
+          <div className="Mission">
+            <h2>MISSION</h2>
+            <div className="MissionLogo">
+              <p>
+                <FontAwesomeIcon
+                  icon={faQuoteLeft}
+                  style={{ position: "relative", top: "-20px", left: "-20px" }}
+                />
+                To enhance the movie experience by
+                <br />
+                simplifying snack ordering with technology.
+                <FontAwesomeIcon
+                  icon={faQuoteRight}
+                  style={{
+                    position: "relative",
+                    bottom: "-50px",
+                    right: "-20px",
+                  }}
+                />
+              </p>
+            </div>
           </div>
         </div>
-        <NavFooter />
       </div>
+      <NavFooter />
     </>
   );
 };
